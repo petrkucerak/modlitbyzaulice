ca329f64457f91b772a72ed0dcf0082e57a2cea8
petrkucerak <petr@khome.cz>
2024-07-14 20:30:36 +0200

    Add space to header and footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="border-t w-[90vw] max-w-[800px] flex flex-col items-center justify-between">
-      <p>Projekt Modlitba za Pardubice podporují</p>
-      <p className="text-sm">
+      <p className="mt-4">Projekt Modlitba za Pardubice podporují</p>
+      <p className="text-sm mb-4">
         Tento projekt je vytvořený jako open-source. Pro více informací mrkni do{" "}
         <Link
           href={"https://github.com/petrkucerak/modlitbazapardubice"}
--- components/layout/header.js
+++ components/layout/header.js
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="border-b w-[90vw] max-w-[800px] flex flex-col items-center justify-between">
-      <Link href={"/"}>Modlitba za Pardubice</Link>
+      <Link href={"/"} className="my-4">
+        Modlitba za Pardubice
+      </Link>
     </header>
   );
 }
