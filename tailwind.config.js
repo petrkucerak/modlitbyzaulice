8ed7bd22e578e01f47c8cd3692d592f169439d4c
petrkucerak <petr@khome.cz>
2024-09-14 20:30:39 +0200

    Create color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
         brother1816: ["Brother 1816"],
         eigerdals: ["Eigerdals"],
       },
+      colors: {
+        black: "#000000", // Black
+        blue: "#3d8bc9", // Light Blue
+        white: "#ede1c7", // White
+        darkBlue: "#516ba8", // Dark Blue
+        wine: "#5a003d", // Wine
+        red: "#ea4756", // Red
+        pink: "#eb8fc2", // Pink
+        olive: "#789d3d", // Olive
+        teal: "#00846d", // Teal
+        yellow: "#f8e447", // Yellow
+      },
     },
   },
   plugins: [],
