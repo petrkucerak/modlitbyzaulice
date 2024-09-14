4f2fc7a38bde0fa9e1b88362834c2827e427147a
petrkucerak <petr@khome.cz>
2024-09-14 20:09:59 +0200

    Create classic menu for PC

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,17 +1,27 @@
+"use client";
 import Link from "next/link";
+import IconCustomLogo from "../icons/logo";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="border-b w-[90vw] max-w-[900px] flex flex-col items-center justify-between">
+    <header className="w-[100vw] flex flex-row items-center justify-between">
       <Link href={"/"} className="my-4">
-        Modlitby za Pardubice
+        <IconCustomLogo className={"w-[2rem] ml-[1.8rem]"} />
       </Link>
-      <div>
+      <div className="max-w-[600px] flex flex-row items-center justify-between w-full">
         <Link href={"/"}>o projektu</Link>
         <Link href={"/"}>jak se zapojit</Link>
         <Link href={"/"}>mapa</Link>
         <Link href={"/"}>podporují</Link>
       </div>
+      <div>burger menu</div>
     </header>
   );
 }
