5482d33acecb48eaca4abf6d2427dcfcb1dc96f6
petrkucerak <petr@khome.cz>
2024-09-15 11:53:32 +0200

    Add content to the footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,12 +2,18 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full flex flex-col items-center justify-between bg-teal">
+    <footer className="w-full flex flex-col items-center justify-between bg-teal text-white">
       <p className="text-sm w-[90vw] max-w-[800px] mt-[2rem]">
         Pokud byste chtěli podpořit projekt svým časem nebo finančně, napiště
         nám na{" "}
         <Link href={"mailto:info@modlitbyzapardubice.cz"} className="underline">
           info@modlitbyzapardubice.cz
+        </Link>
+        .
+      </p>
+      <p className="text-sm w-[90vw] max-w-[800px]">
+        <Link href={"/"} target="_blank" className="underline">
+          Pravidla akce a zásady ochrany osobních údaju
         </Link>
         .
       </p>
@@ -19,6 +25,13 @@ export default function Footer() {
         , vývoj{" "}
         <Link href={"TODO"} className="">
           Petr Kučera
+        </Link>
+        .
+      </p>
+      <p className="text-sm w-[90vw] max-w-[800px]">
+        Organizátorem akce je{" "}
+        <Link href={""} target="_blank">
+          Římskokatolická farnost Pardubice
         </Link>
         .
       </p>
