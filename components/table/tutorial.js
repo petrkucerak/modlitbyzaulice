79b55f75293d39e29cf4b1d929f3bfe6902935e2
petrkucerak <petr@khome.cz>
2024-10-08 17:10:58 +0200

    Add content

--- components/table/tutorial.js
+++ components/table/tutorial.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Tutorial({ className }) {
   return (
     <div className={`${className}`}>
-      <h2 className="font-brother1816 text-2xl font-semibold uppercase">
+      <h2 className="font-brother1816 text-2xl font-semibold uppercase text-darkBlue">
         Jak nahrát změněný soubor na web
       </h2>
-      <h3 className="text-xl font-brother1816 mt-6 font-medium uppercase">
+      <h3 className="text-xl font-brother1816 mt-6 font-semibold uppercase text-olive">
         (A) Vyplňte tabulku se změnami
       </h3>
       <p className="text-lg my-2">
@@ -13,18 +15,30 @@ export default function Tutorial({ className }) {
       </p>
       <ol className="text-lg list-decimal pl-6">
         <li>
-          V rohu každé kartičky najdete její identifikační číslo (ID). Číslo je
-          na straně s jménem toho, kdo se za ulici modlil.
+          <strong className="font-medium">V rohu kartičky</strong> najdete její{" "}
+          <strong className="font-medium">identifikační číslo</strong> (ID).
+          Číslo je na straně s jménem toho, kdo se za ulici modlil.
         </li>
-        <li>Číslo zadejte do formuláře pro vyhledávání.</li>
         <li>
-          Stiskněte ENTER a přejděte tak do pole pro zadání jména modlitebníka
-          či modlitebníků.
+          <strong className="font-medium">Číslo</strong> zadejte{" "}
+          <strong className="font-medium">do formuláře</strong> pro vyhledávání.
         </li>
-        <li>Opět stiskněte ENTER a vrátíte se tak zpět do vyhledávání. </li>
-        <li>Přejděte k další kartičce a postupujte opět od začátku.</li>
+        <li>
+          Stiskněte ENTER a přejděte tak do pole pro zadání{" "}
+          <strong className="font-medium">jména modlitebníka</strong> či
+          modlitebníků.
+        </li>
+        <li>
+          Opět stiskněte <strong className="font-medium">ENTER</strong> a
+          vrátíte se tak{" "}
+          <strong className="font-medium">zpět do vyhledávání.</strong>{" "}
+        </li>
+        <li>
+          Přejděte k <strong className="font-medium">další kartičce</strong> a
+          postupujte opět od začátku.
+        </li>
       </ol>
-      <h3 className="text-xl font-brother1816 mt-6 font-medium uppercase">
+      <h3 className="text-xl font-brother1816 mt-6 font-semibold uppercase text-olive">
         (B) Soubor stáhněte a nahrajte ho do repositáře
       </h3>
       <p className="text-lg my-2">
@@ -33,17 +47,31 @@ export default function Tutorial({ className }) {
         následujícími kroky:
       </p>
       <ol className="text-lg list-decimal pl-6">
-        <li>Stáhněte si soubor pomocí tlačítka Stáhnout soubor.</li>
         <li>
-          Přihlaste se ke svému účtu na Githubu a nahrajte soubor do složky
-          data. Nejjednodušší způsob je přetažením. Pozor, soubor se musí
-          jmenovat streets_with_coordinates.js
+          <strong className="font-medium">Stáhněte si soubor</strong> pomocí
+          tlačítka Stáhnout soubor.
         </li>
         <li>
-          Vytvořte pull-request, a zkontrolujte si, jestli se dané změny
-          projevili na mapě.
+          Přihlaste se ke svému účtu na Githubu a{" "}
+          <strong className="font-medium">nahrajte soubor</strong> do složky{" "}
+          <Link
+            href={""}
+            target="_blank"
+            className="font-medium text-stone-700 underline"
+          >
+            data
+          </Link>
+          . Nejjednodušší způsob je přetažením. Pozor, soubor se musí jmenovat{" "}
+          <div className="font-mono">streets_with_coordinates.js</div>
         </li>
-        <li>Pull-request potvrďte a nahrajte ho do produkční verze.</li>
+        <li>
+          <strong className="font-medium">Vytvořte pull-request</strong>, a
+          zkontrolujte si, jestli se dané změny projevili na mapě.
+        </li>
+        <li>
+          <strong className="font-medium">Pull-request squashnete</strong> a
+          nahrajte ho do produkční verze.
+        </li>
       </ol>
     </div>
   );
