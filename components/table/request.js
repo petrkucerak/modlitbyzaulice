6b02fa1d0f51b8fdd473e7a6adba71740ac11405
Petr Kučera <petr@khome.cz>
2024-10-08 15:41:20 +0200

    Update request (#28)
    * Update request.js
    
    * Update barnabas-bot.yml

--- components/table/request.js
+++ components/table/request.js
@@ -1,32 +1,38 @@
 import Swal from "sweetalert2";
 
 export async function createRequest(exportObj, userName) {
-  let content = [];
-  exportObj.map((street) => {
-    content.push({ unique_number: street.unique_number, name: street.name });
-  });
+  // Function to split the array into chunks
+  const chunkArray = (array, chunkSize) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
 
-  const body = `
-   {
-     "content": ${JSON.stringify(exportObj)},
-     "username": "${userName}"
- }
-   `;
+  // Split the exportObj into chunks of, e.g., 20 streets per request
+  const chunks = chunkArray(exportObj, 20);
 
-  const response = await fetch("https://api.modlitbyzaulice.cz/", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: body,
-  });
+  for (const chunk of chunks) {
+    const body = JSON.stringify({
+      content: chunk,
+      username: userName,
+    });
 
-  response.json().then((data) => {
+    const response = await fetch("https://api.modlitbyzaulice.cz/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: body,
+    });
+
+    const data = await response.json();
     if (data.status === "OK") {
       Swal.fire({
         title: "Barabáš!",
-        text: "BOT Barabáš úspěšně vytvořil žádost o přidání nově promodlených ulic. Žádost najdeš zde: <a href='https://github.com/petrkucerak/modlitbyzaulice/pulls' target='_blank'>https://github.com/petrkucerak/modlitbyzaulice/pulls</a>.",
+        text: `BOT Barabáš úspěšně vytvořil žádost. Branch name: ${data.branch_name}. Pull request here: <a href='https://github.com/petrkucerak/modlitbyzaulice/pull/${data.branch_name}' target='_blank'>View Pull Request</a>.`,
         icon: "success",
         confirmButtonColor: "#3d8bc9",
       });
@@ -37,6 +43,7 @@ export async function createRequest(exportObj, userName) {
         icon: "error",
         confirmButtonColor: "#3d8bc9",
       });
+      break;
     }
-  });
+  }
 }
